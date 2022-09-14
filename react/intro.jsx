b51a9d9a0d625617cb5b25671a2d52e115266cb3
s-harsh <harshvardhansonihv@gmail.com>
2022-09-14 23:21:33 +0530

    Intro to React

---
+++ react/intro.jsx
@@ -0,0 +1,18 @@
+// React makes building complex interractive and reactive user interfaces 
+
+/* Build something with React you won't focuse on all the nitty-gitty
+detials and it will be less error prone, because you become able to focuse
+on your core business logic that makes up your application
+intead of having to focus on the actual steps of updating the page 
+when something happens somewhere.
+That's why we use libraries like React
+React embraces a concept called components. React is all about 
+components. Because all user interfaces in the end are made up of coponents.
+Components are resuable building blocks in your interface, they are in the end
+just a combination of HTML code,CSS code for styling & possibly 
+JavaScript code for some logic.
+COmponents advantages -
+1. Reusability reduce reputation 
+2. Separation of Concerns - Don't do too manu things in one & the same place
+3. 
+ */
