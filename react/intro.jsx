10558dbd165906ffc1767695d2496dec4d7bc595
s-harsh <harshvardhansonihv@gmail.com>
2022-09-15 10:51:00 +0530

    NPM Know about npm.

--- react/intro.jsx
+++ react/intro.jsx
@@ -21,5 +21,17 @@ HTML and JavaScript(and CSS).
 React use Declarative Approach for building these components means define the
 desired target state and let React figure out the actual JavaScript DOM instructions.
 
+Nodejs all javascript code outside of the browser, it is not something at all in 
+React, becauuse React code is js code that runs in the browser.
+We need nodejs for this Create React App tool.
 
+NPM - Node Package Manager
+NPM is a package manager for JavaScript. It is the default package manager that comes
+with NodeJS when you install it.
+ReactJS uses Node.js a JavaScript runtime, to build your JacaScript code.
+Basically the ReactJS is a JavaScript framework which needs the help of nodejs and npm
+to tell the native side(Android/iOS) that these are packages I need to run my app
+And it adds all the dependencies needed to run the app.
+
+Bacially you don't need node.js to use React on the client side
  */
