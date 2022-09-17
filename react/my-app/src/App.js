fd95cfb54791302b8afc82ed000a8d35dc65b5d6
s-harsh <harshvardhansonihv@gmail.com>
2022-09-17 10:25:07 +0530

    Learning update the ract app

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const para=document.createElement('p');
+  para.textContent='This is harsh by js';
+  document.getElementById('root').append(para);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <p>
+          This is Harsh!
         </p>
         <a
           className="App-link"
