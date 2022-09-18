f73f4715cf20739594a01005bec3579446e2dc9a
s-harsh <harshvardhansonihv@gmail.com>
2022-09-18 11:51:47 +0530

    Adding CSS in JSX code

--- react/my-app/src/ExpenseItem.js
+++ react/my-app/src/ExpenseItem.js
@@ -1,12 +1,14 @@
-
+import './ExpenseItem.css'
 function ExpenseItem(){
     return (
         // writing more complex JSX Code
         // Note - must have a root 
-        <div>
-            <div>Data</div>
-            <div>Tiltle</div>
-            <h1>Hello JSX</h1>
+        <div className ="expense-item">
+            <div>18th Sept 2022 </div>
+            <div className="expense-item__description" >
+                <div>Car Insurance </div>
+                <h1 className="expense-item__price" > Price =100 $</h1>
+            </div>
         </div>
     );
 };
