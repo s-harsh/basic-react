9c3ec036a70b5b8e460047a4d680e3949a5ff263
s-harsh <harshvardhansonihv@gmail.com>
2022-09-20 10:02:32 +0530

    Learn Passing Data via Props We can directly use javascript call by value using props and make data dynamically.

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './ExpenseItem';
-import ExpenseItem1 from './ExpenseItem1';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseItem1 from "./ExpenseItem1";
 
 function App() {
-  const para=document.createElement('p');
-  para.textContent='This is harsh by js';
-  document.getElementById('root').append(para);
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 300,
+      date: new Date(2022, 8, 20),
+    },
+    {
+      title: "Bike Insurance",
+      amount: 400,
+      date: new Date(2022, 8, 20),
+    },
+    {
+      title: "Scooty Insurance",
+      amount: 500,
+      date: new Date(2022, 8, 20),
+    },
+  ];
+  const para = document.createElement("p");
+  para.textContent = "This is harsh by js";
+  document.getElementById("root").append(para);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,10 +32,22 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <ExpenseItem></ExpenseItem>
-        <ExpenseItem1></ExpenseItem1>
-        <p>
-          This is Harsh!
-        </p>
+        <ExpenseItem1
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem1>
+        <ExpenseItem1
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpenseItem1>
+        <ExpenseItem1
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        ></ExpenseItem1>
+        <p>This is Harsh!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- react/my-app/src/ExpenseItem1.js
+++ react/my-app/src/ExpenseItem1.js
@@ -2,16 +2,13 @@
 Here just we use baasic var
 If we want to  add javaScript in the JSX code we use this {} */
 import './ExpenseItem.css'
-function ExpenseItem1(){
-    const expenseDate=new Date(2022,8,19);
-    const expneseTitle='Car Insurance';
-    const expneseAmount=300;
+function ExpenseItem1(props){
     return(
         <div className="expense-item">
-            <div>{expenseDate.toISOString()} </div>
+            <div>{props.date.toISOString()} </div>
             <div className='expense-item__description'>
-                <h2>{expneseTitle}</h2>
-                <div className='expense-item__price'>{expneseAmount}$</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}$</div>
             </div>
         </div>
     );
