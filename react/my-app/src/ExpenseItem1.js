6e1957a65ad27912d077f825bce8fdd9ed7ab18e
s-harsh <harshvardhansonihv@gmail.com>
2022-12-12 17:28:59 +0530

    no message

--- react/my-app/src/ExpenseItem1.js
+++ react/my-app/src/ExpenseItem1.js
@@ -5,15 +5,24 @@ If we want to  add javaScript in the JSX code we use this {} */
 /* We use props in React to pass data from one component to another (from a parent component to a child component(s)).
 Props is just a shorter way of saying properties. They are useful when you want the flow of data in your app to be dynamic. */
 
+import React,{useState} from 'react';
+// between the curly brance we can import exact specifically named things
+/* useState function all to define values as state, where changes to these value should reflect in comp function
+Use State is a hook in react (all react hooks can be recognize by the fact that they start with the word 'use')
+All the hooks must called inside the component function like - ExpenseItem, they can't be called outside of these funciton
+And they shouldn't called in a nested function
+
+useState is a Hook that lets you add React state to function components*/
 import Card from './card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  let title=props.title;
-
+  // let title=props.title;
+  const [title,setTitle]=useState(props.title);
+  
   const clickHandler=()=>{
-    title='Updated!';
+    setTitle('Updated');
     console.log(title);
   }
 
@@ -25,6 +34,9 @@ function ExpenseItem(props) {
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
+      {/* Handling events with React elements is very similar to handling events on DOM elements.
+      React events are named using camelCase, rather than lowercase.
+      With JSX you pass a function as the event handler, rather than a string. */ }
     </Card>
   );
 }
